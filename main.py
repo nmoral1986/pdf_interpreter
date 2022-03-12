454f49cfd9d117562e97e9225d5d9741fcbbad7b
Estebrock09 <estebanquevedo.p@gmail.com>
2022-03-11 21:36:10 -0500

    [ADD] Ds function

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import tabula
+from tika import parser
 
 
 def read_file(pdf_file):
-    table = tabula.read_pdf(pdf_file, pages=3)
-    print(table[0])
+    data = {}
+    file_data = parser.from_file(pdf_file)
+    text = file_data['content']
+    new_text = text.split('\n')
+    for line in new_text:
+        if (('DSR-' or 'DS-') and '$') in line:
+            s_line = line.split('$')
+            dsr_list = s_line[0].split(' ')
+            dsr = dsr_list[0]
+
+            val_list = s_line[1].split(' ')
+            val = val_list[1]
+            if 'DSR-' in dsr or 'DS-' in dsr:
+                data[dsr] = val
+    print(data)
 
 
 if __name__ == "__main__":
